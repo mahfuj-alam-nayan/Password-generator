7ba8ac251db8d5e2966beadeddad6138781211e9
mahfuj-alam-nayan <mdn566332@gmail.com>
2026-06-26 23:09:03 +0600

    Add files via upload

---
+++ Post Increment (a++).c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 10;
+
+    printf("Before increment: %d\n", a);
+
+    printf("Using a++: %d\n", a++);
+
+    printf("After increment: %d\n", a);
+
+    return 0;
+}
