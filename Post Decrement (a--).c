6604806ff2008c193793883141ac4b52dd1fd82b
mahfuj-alam-nayan <mdn566332@gmail.com>
2026-06-26 23:10:43 +0600

    Add files via upload

---
+++ Post Decrement (a--).c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 10;
+
+    printf("Before decrement: %d\n", a);
+
+    printf("Using a--: %d\n", a--);
+
+    printf("After decrement: %d\n", a);
+
+    return 0;
+}
